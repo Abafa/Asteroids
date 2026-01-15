d3c070d3c852a49bc047cc7871efca1dfaa31c38
Abafa <flo.beg@free.fr>
2026-01-15 17:55:24 +0100

    ca marche meh. on verra mieux demain

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,12 +1,18 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)  
+
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,6 +27,7 @@ def main():
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+        player.draw(screen)
         
 
 
